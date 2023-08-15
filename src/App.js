b46dbfffe5cd738f752f2d25c186e7bce495db7d
Pooja <pkumawat009876@gmail.com>
2023-08-15 17:47:13 +0530

    first part completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItems from './components/ExpenseItems';
-import './App.css';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -25,38 +24,11 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      
-      <ExpenseItems
-      title={expenses[0].title}
-     amount={expenses[0].amount}
-     date = {expenses[0].date}/>
-  
-     <ExpenseItems
-      title={expenses[1].title}
-     amount={expenses[1].amount}
-     date = {expenses[1].date}>
-     </ExpenseItems>
-     <ExpenseItems
-      title={expenses[2].title}
-     amount={expenses[2].amount}
-     date = {expenses[2].date}>
-     </ExpenseItems>
-     <ExpenseItems
-      title={expenses[3].title}
-     amount={expenses[3].amount}
-     date = {expenses[3].date}>
-     </ExpenseItems>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
-// function User() {
-//   return (
-//     <div>
-//       <h1>Expense</h1>
-//       <ExpenseItems/>
-//     </div>
-//   )
-// }
 
 export default App;
