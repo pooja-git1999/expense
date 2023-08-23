b1a447f8ff325ddcb8e1e81e4599390c56233d2e
Pooja <pkumawat009876@gmail.com>
2023-08-23 18:15:30 +0530

    rendering list dynamic

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -7,8 +7,8 @@ export default function ExpenseDate(props) {
     return (
         <div className="expense-date">
             <div className="expense-date__month">{month}</div>
-            <div className="expense-date__year">{day}</div>
-            <div className="expense-date__day">{year}</div>
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__year">{year}</div>
         </div>
     )
 }
